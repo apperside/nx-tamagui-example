2a33a18309117a29373a04d819524dad44747647
apperside <info@apperside.com>
2022-07-22 15:36:52 +0200

    add tamagui babel plygin

---
+++ apps/my-app/babel.config.js
@@ -0,0 +1,27 @@
+process.env.TAMAGUI_TARGET = 'native'
+module.exports = (api) => {
+  api.cache(false);
+  return {
+    plugins: [
+      [
+        '@tamagui/babel-plugin',
+        {
+          components: ['tamagui'],
+          config: 'tamagui.config.ts',
+          importsWhitelist: ['constants.js', 'colors.js'],
+          logTimings: true,
+          disableExtraction: process.env.NODE_ENV === 'development',
+        },
+      ],
+      // be sure to set TAMAGUI_TARGET
+      [
+        'transform-inline-environment-variables',
+        {
+          include: 'TAMAGUI_TARGET',
+        },
+      ],
+    ],
+
+    presets: ['module:metro-react-native-babel-preset'],
+  };
+};
