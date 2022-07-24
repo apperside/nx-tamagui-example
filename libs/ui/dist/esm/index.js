5ddb2662eda9b9f50f199ab08c3403824b19675a
apperside <info@apperside.com>
2022-07-24 14:57:20 +0200

    remove dist folder from source control

--- libs/ui/dist/esm/index.js
+++
@@ -1,7 +0,0 @@
-export * from "tamagui";
-export * from "./MyComponent";
-export * from "./MyCard";
-export * from "./AppProvider";
-export * from "./UserDetails";
-export * from "./HomePage";
-//# sourceMappingURL=index.js.map
--- libs/ui/dist/jsx/MyCard.js
+++
@@ -1,8 +0,0 @@
-import { Stack } from "tamagui";
-function MyCard({ children, ...props }) {
-  return <Stack br="$4" {...props} bc="red" minHeight={10}>{children}</Stack>;
-}
-export {
-  MyCard
-};
-//# sourceMappingURL=MyCard.js.map
